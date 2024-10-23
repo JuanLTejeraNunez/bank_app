f20bc55b0c3d34f8c07cc526cc86b1567bf80943
JuanLTejeraNunez <jltejera.nunez@outlook.com>
2024-10-22 21:51:32 -0400

    c3
    ending main page gui fase 1

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
 
 const TotalBalanceBox = ({accounts=[],totalBanks,totalCurrentBalance}:TotalBalanceBoxProps) => {
+   
+
   return (
     <section className="total-balance">
         <div className="total-balance-chart">
@@ -12,6 +15,16 @@ const TotalBalanceBox = ({accounts=[],totalBanks,totalCurrentBalance}:TotalBalan
             <h2 className="header-2">
             Banks Accounts : {totalBanks}
             </h2>
+
+            <div className="flex flex-col gap-2">
+                <p className="total-balance-lavel">
+                    Total Current Balance
+                </p>
+                <p className="total-balance-amount flex-center gap-2">
+                    <AnimatedCounter amount={totalCurrentBalance}/>
+                    
+                </p>
+            </div>
         </div>
     </section>
   )
