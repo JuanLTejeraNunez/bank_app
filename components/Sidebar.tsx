9df2bd3f9d10a17e82b88428f5efab6e99d6304f
JuanLTejeraNunez <jltejera.nunez@outlook.com>
2024-10-27 20:10:13 -0400

    c6
    sidebar items css

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,12 @@
-
-import link from 'next/link'
+'use client'
+import Link from 'next/link'
 import Image from 'next/image'
-
+import { sidebarLinks } from '@/constants'
+import { cn} from '@/lib/utils'
+import { userPathname} from 'next/navegation'
 
 const Sidebar = ({user}: SiderbarProps) => {
+  const pathname  = userPathname();
   return (
     <section className="sidebar">
         <nav className="flex flex-col gap-4">
@@ -18,8 +21,30 @@ const Sidebar = ({user}: SiderbarProps) => {
                  className="size-[24px] max-xl:size-14"
                  />
 
+               <h1 className="sidebar-logo">Horizon</h1>
             </link>
-            <h1 className="sidebar-logo">Horizon</h1>
+           {sidebarLinks.map((item)=>{
+                   const isActive = pathname===item.route || pathnamestr.startsWith(`${item.route}/`)
+            return{
+                <Link
+                 href={item.route}
+                key={item.label}
+                className={cn ('siderbar-link', {'bg-bank-gradient': isActive})}
+                >
+                <div className='relative size-6'>
+                <Image
+                src={item.IMGurl}
+                alt={item.label}
+                fill
+                className={cn({'brightness=[3] invert- 0': isActive})}
+                />
+                </div>
+               {item.label}
+</Link>
+              
+            }
+           })}
+
         </nav>
 
     </section>
