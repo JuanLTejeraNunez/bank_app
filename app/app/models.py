4c32e8a7aef06c0863407fbc1b47815383b40aa9
JuanLTejeraNunez <105330351+JuanLTejeraNunez@users.noreply.github.com>
2026-06-24 11:22:28 -0400

    Delete app/app directory

--- app/app/models.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, Numeric
-from app.database import Base
-
-class Account(Base):
-    __tablename__ = "accounts"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    balance = Column(Numeric(12,2), nullable=False, default=0)
